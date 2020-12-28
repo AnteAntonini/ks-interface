458594529a707fa656b892193a94e395b6b134e8
Dalibor Nerber <dalibor.nerber@gmail.com>
2020-12-28 14:04:17 +0100

    Imported store

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,20 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         user: null
-    }
+    },
+    getters: {
+        user: (state) => {
+            return state.user
+        }
+    },
+    actions: {
+        user(context, user) {
+            context.commit('user', user)
+        }
+    },
+    mutations: {
+        user(state, user) {
+            state.user = user
+        }
+    },
 })
